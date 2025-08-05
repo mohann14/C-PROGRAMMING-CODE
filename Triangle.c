c346404823fd11564b3e400669fb0b713455964d
mohann14 <skmohansai1406@gmail.com>
2025-08-05 10:49:24 +0530

    Create Triangle.c

---
+++ Triangle.c
@@ -0,0 +1,11 @@
+#include
+int main() { 
+int a,b,c;
+scanf("%d %d %d",&a,&b,&c); 
+if (a>b && a>c)
+printf("A is biggest...\n") ;
+else if (b>a && b>c)
+printf("B is biggest...\n");
+else
+printf("C is biggest...\n"); return 0;
+           }
